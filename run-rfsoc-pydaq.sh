765e2ac090e77bd5ec1106bf199a28a91287a471
Patrick Allison <allison.122@osu.edu>
2024-02-16 16:50:45 +0000

    add run-rfsoc-pydaq.sh for simplicity

---
+++ run-rfsoc-pydaq.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this is awkward because Pynq needs root access,
+# but in addition it installs its own version of Python
+# in a location that only is sourced on login shells,
+# so we have to do sudo -i, which means we also lose
+# our current directory.
+
+CURDIR=`pwd`
+sudo xauth add $(xauth -f ~xilinx/.Xauthority list|tail -1)
+sudo -i DISPLAY=$DISPLAY python3 $CURDIR/rfsoc-pydaq.py
