ed7fabb0f20771c086de9739ae0d3dbaeb815f32
HIP99 <hugo.pumphrey.22@ucl.ac.uk>
2024-03-26 16:22:53 +0000

    Added functionality

--- rfsoc-pydaq.py
+++ rfsoc-pydaq.py
@@ -1,60 +1,231 @@
+##Effectively a backup because I want to move some things arround and the what not
+
+##Python Imports
 import numpy as np
 import tkinter as tk
 from tkinter import filedialog
+from scipy.fft import fft, ifft, fftfreq
+from scipy.constants import speed_of_light
 
+#System Imports
 import logging
 import os, sys, inspect, importlib
 
+#Module Imports
 from textconsole.TextConsole import TextConsole
 from scrolledlog.ScrolledLog import ScrolledLog
 from waveframe.Waveframe import Waveframe
 
 logger = logging.getLogger(__name__)
 
-# GLOBAL CONFIGS
-# THESE ARE STORED IN THE RFSoC_Daq CLASS
-# BUT ARE HERE SO THEY CAN BE CHANGED EASY
-numChannels = 4
-numSamples = 2048
+#Establishing base values for parameters
+numChannels = 4     ##Probably shouldn't change
+sampleExponent = 5#11
+numSamples = 2**sampleExponent#2**12#2**11
 sampleRate = 3.E9
+figsize = (3,2)
 
+portNames = ["ADC224_T0_CH0 (LF)","ADC224_T0_CH1 (LF)","ADC225_T1_CH0 (HF)","ADC225_T1_CH1 (HF)","","","",""]
+
+##Allows it to be globally accessible
 theDaq = None
 
-# THIS IS THE GLOBAL CLASS
-# It gets passed to the internal Python console,
-# so it has stuff stored in it.
-# It internally holds the top displays.
+
+#GUI classe(s)
+class submitButton:
+    def __init__(self,
+                 master,
+                 frame,
+                 label_text,
+                 placeholder_text,
+                 submit_command):
+        
+        self.frame=frame
+        
+        self.label =tk.Label(self.frame, text=label_text)
+        self.label.pack(side=tk.LEFT)
+        
+        self.entry = tk.Entry(self.frame, width=12)
+        self.entry.insert(0, placeholder_text)
+        self.entry.pack(side=tk.LEFT)
+        
+        self.submit_button = tk.Button(self.frame, text="Submit", command=submit_command)
+        self.submit_button.pack(side=tk.LEFT)
+
+#FPGA Class
 class RFSoC_Daq:
     def __init__(self,
                  frame,
                  numChannels = 4,
                  numSamples = 2048,
-                 sampleRate = 3.E9
-                 ):
+                 sampleRate = 3.E9,
+                 figsize = (3,2)):
+        
+        #Inputs
+        self.frame = frame
         self.numChannels = numChannels
         self.numSamples = numSamples
-        self.frame = frame
+        self.sampleRate = sampleRate
+        self.figsize = figsize
+        #Inititalising instance attributes
         self.adcBuffer = np.zeros( (numChannels, numSamples), np.int16 )
         self.dev = None
+        self.plotFreq = True
+        self.plotFit = True
         self.wf = []
-        for i in range(numChannels):
-            thisWf = Waveframe(self.frame, sampleRate)
+        #Made this a method so I could reset it from mainloop
+        self.startWaveFrame()
+        
+    def startWaveFrame(self):
+        for i in range(self.numChannels):
+            # print(self.plotFreq, self.plotFit)
+            thisWf = Waveframe(self.frame, self.sampleRate, self.figsize)
             self.wf.append(thisWf)
             thisWf.pack(side = tk.LEFT )
+    
+    ##Sets
+    def setNumChannels(self, Channels = 4):
+        self.numChannels = Channels
+        logger.debug(f"You will now record {self.numChannels} channels")
+        return f"You will now record {self.numChannels} channels"
+    def setNumSamples(self, NSum = 2**11):
+        self.numSamples = NSum
+        logger.debug(f"You will now take {self.numSamples} samples")
+        return f"You will now take {self.numSamples} samples"
+    def setSampleRate(self, rate = 3*10**9):
+        self.sampleRate = rate
+        logger.debug(f"Sample rate is now {self.sampleRate/10**9} GSPS")
+        return f"Sample rate is now {self.sampleRate/10**9} GSPS"
+    def setFigSize(self, sizing = (3,2)):
+        self.figsize = sizing
+        logger.debug(f"The figure size is now {self.figsize}")
+        return f"The figure size is now {self.figsize}"
+    def setAdcBuffer(self):
+        self.adcBuffer = np.zeros( (theDaq.numChannels, theDaq.numSamples), np.int16 )
+        logger.debug("The adcBuffer has hopefully been updated")
+        return 'adcBuffer Updated'
+    def setWaveFrame(self):
+        self.startWaveFrame()
+        logger.debug("Waveframe should be updated")
+        return 'WaveFrame Updated'
+    def setPlotFreq(self, Value=True):
+        if isinstance(Value, bool):
+            self.plotFreq = Value
+            self.setWaveFrame()
+            logger.debug(f"{'Plotting' if Value else 'Not plotting'} the frequency")
+            return f"{'Plotting' if Value else 'Not plotting'} the frequency"
+        else:
+            logger.debug("Please input a valid option")
+            return"Please input a valid option"
+    def setPlotFit(self, Value=True):
+        if isinstance(Value, bool):
+            self.plotFit = Value
+            self.setWaveFrame()
+            logger.debug(f"{'Plotting' if Value else 'Not plotting'} the fitted curve")
+            return f"{'Plotting' if Value else 'Not plotting'} the fitted curve"
+        else:
+            logger.debug("Please input a valid option")
+            return"Please input a valid option"
+    
+    ##Gets
+    def getNumChannels(self):
+        logger.debug(f"You are recording from {self.numChannels} channels")
+        return self.numChannels
+    def getNumSamples(self):
+        logger.debug(f"You are taking {self.numSamples} channels")
+        return self.numSamples
+    def getNumSamples(self):
+        logger.debug(f"You are recording {self.sampleRate/10**9} GSPS")
+        return self.sampleRate
+    def getNumSamples(self):
+        logger.debug(f"You displaying figures on a {self.figsize} grid")
+        return self.figsize
+    def getAdcBuffer(self):
+        logger.debug(f"There is no way I'm printing the adcBuffer. That thing is huge")
+        return self.adcBuffer
+    def getPlotFreq(self):
+        Value = self.plotFreq
+        logger.debug(f"You are currently {'Plotting' if Value else 'Not plotting'} the frequency")
+        return Value
+    def getPlotFit(self):
+        Value = self.plotFit
+        logger.debug(f"You are currently {'Plotting' if Value else 'Not plotting'} the fitted waveform")
+        return Value
+    
+    ##Processes
+    def convertToMag(self, yf):
+        N = len(yf)
+        return 2.0/N * np.abs(yf[0:N//2])
+    
+    ##Maybe make amp and freq into one method since they basically do the same thing
+    def calcWave(self, Ch=0):
+        if isinstance(Ch, int) and 0 <= Ch < self.numChannels:
+            signal = self.adcBuffer[Ch]
+            N = len(signal)
+            dt = 1 / self.sampleRate
+            df = 1 / (N * dt)
+            fft_result = fft(signal)
+            xf = np.linspace(0.0, 1.0 / (2 * dt), N // 2)
+            mag_spectrum = np.abs(fft_result[:N//2])
+            phase_spectrum = np.angle(fft_result[:N//2])
+            peak_freq_index = np.argmax(mag_spectrum)
+            frequency = xf[peak_freq_index]
+            amplitude = mag_spectrum[peak_freq_index] * 2 / N
+            phase = phase_spectrum[peak_freq_index]
+            logging.debug(f"Amplitude: {amplitude:.3f} ADC counts || Frequency: {(frequency/10**6):.3f} MHz || Phase: {phase:.3f} rad || Channel: {Ch}")
+            return amplitude, frequency, phase
+        else:
+            raise ValueError('Not an available channel')
+        
+    def getWL(self, Ch=0):
+        if isinstance(Ch, int) and 0<=Ch<self.numChannels:
+            wavelength = speed_of_light/self.calcWave(Ch)[1]
+            logging.debug(f"Wavelength : {wavelength} of Channel : {Ch}")
+            return wavelength
+        else:
+            return 'Not an available channel'
+        
+    ##Prints
+    def printWave(self, Ch=-1):
+        if isinstance(Ch, int) and 0<=Ch<self.numChannels:
+            amplitude, frequency = calcWave(Ch)
+            print(f"Amplitude : {amplitude} ADC counts || Frequency : {frequency/10**6} MHz || Channel : {Ch}")
+        elif Ch==-1:
+            for i in range(self.numChannels):
+                amplitude, frequency = calcWave(Ch)
+                print(f"Channel {i} : Amplitude : {amplitude} ADC counts || Frequency : {frequency/10**6} MHz")
+        else:
+            return 'Not an available channel'
+        
+    def printWavelength(self, Ch=-1):
+        if isinstance(Ch, int) and 0<=Ch<self.numChannels:
+            wavelength = getWL(Ch)
+            print(f"Wavelength : {wavelength} metres || Channel : {Ch}")
+        elif Ch==-1:
+            for i in range(self.numChannels):
+                wavelength = getWL(Ch)
+                print(f"Channel {i} : Wavelength : {wavelength} metres")
+        else:
+            return 'Not an available channel'
 
+##Not sure why this exists but hey ho
 def defaultUserCommand():
     return
 
-
-def rfsocLoad():
+def rfsocLoad(hardware = ""):
     """Load an overlay file describing an RFSoC instance.
     The overlay needs to support being created bare (just "overlayName()")
     and must support the "internal_capture( buffer, numChannels )"
     function.
     """    
-    file_path = filedialog.askopenfilename(title="Select an overlay module",
+    if hardware == "zcumts":
+        ##Ammend this location to your leisure
+        file_path = '/home/xilinx/python/zcumts.py'
+    else:
+        file_path = filedialog.askopenfilename(title="Select an overlay module",
                                            filetypes=[("Python files","*.py"),
                                                       ("All files", "*.*")])
+    
     logger.debug("Asked to load overlay at %s" % file_path)
     newdir = os.path.dirname(os.path.abspath(file_path))    
 
@@ -113,24 +284,109 @@ def rfsocLoad():
     os.chdir(curdir)
     return
 
+
 def rfsocAcquire():
     if theDaq.dev is None:
         logger.error("No RFSoC device is loaded!")
+        
     theDaq.dev.internal_capture(theDaq.adcBuffer,
                                 theDaq.numChannels)
+    
     for i in range(theDaq.numChannels):
-        theDaq.wf[i].plot(theDaq.adcBuffer[i])
+        theDaq.wf[i].plot(theDaq.adcBuffer[i], portNames[i], theDaq.calcWave(i),[theDaq.plotFreq, theDaq.plotFit])
 
+
+##Buttons
+def getSubmitInput(value):
+    if isinstance(value, object):
+        logger.debug("Passed in an object, attempting to .entry.get()")
+        result = eval(value.entry.get())
+    else:
+        logger.debug("Passed in an value")
+        result = value
+    return result
+
+
+def submitNumSamples(value = 11):
+    NSamp = getSubmitInput(value)
+    logger.debug(f"Currently have {theDaq.numSamples} samples, you have inputted {2**NSamp} samples")
+    
+    if isinstance(NSamp, int) and 0<NSamp<=14:
+        theDaq.setNumSamples(2**NSamp)
+        theDaq.setAdcBuffer()
+        return f"You will now take {theDaq.numSamples} samples"
+    else:
+        logger.debug("Please input an appropriate Sample Size. The maximum is 14")
+        return 'Please input an appropriate Sample Size'
+    
+def submitSampleRate(value = 3*10**9):
+    rate = getSubmitInput(value)
+    logger.debug(f"Currently have {theDaq.sampleRate} samples, you have inputted {rate} samples")
+    
+    if rate>=10**5:
+        theDaq.setSampleRate(rate)
+        theDaq.setWaveFrame()
+        return f"You will now take {theDaq.sampleRate} GSPS"
+    else:
+        logger.debug("Please input an appropriate Sample Rate")
+        return 'Please input an appropriate Sample Rate'
+    
+def submitNumberOfChannels(value = 4):
+    Channels = getSubmitInput(value)
+    logger.debug(f"Currently have {theDaq.numChannels} samples, you have inputted {Channels} samples")
+    
+    if isinstance(Channels, int) and 0<Channels<=8:
+        theDaq.setNumChannels(Channels)
+        theDaq.setWaveFrame()
+        return f"You will now have {theDaq.numChannels} Channels"
+    else:
+        logger.debug("Please input an appropriate number of channels")
+        return 'Please input an appropriate number of channels'
+    
+def toggle(tg, setFunc):
+    if tg.config('relief')[-1] == 'sunken':
+        tg.config(relief="raised")
+        setFunc(True)
+    else:
+        tg.config(relief="sunken")
+        setFunc(False)
+    return 'Updated plotting'   
+
+##Miscellaneous
+def getAppropriateFigSize():
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    return (screen_width/(100*theDaq.numChannels),screen_height/250)
+
+def reload_script():
+    main_script = sys.argv[0]
+    
+    main_script = main_script.replace('.py', '')
+    
+    if main_script in sys.modules:
+        importlib.reload(sys.modules[main_script])
+    else:
+        logger.debug(f"Module '{main_script}' not found in sys.modules")
+        
+        
 if __name__ == '__main__':
     root = tk.Tk()
+    
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    figsize=(screen_width/(100*numChannels),screen_height/250)
+    
     logging.basicConfig(level=logging.DEBUG)
-    # We have 4 overall frames, just arranged
-    # in a single column, so we can just use
-    # the straight pack geometry manager.
     displayFrame = tk.Frame(master = root,
                             relief = tk.RAISED,
                             borderwidth = 1)
     buttonFrame = tk.Frame(master = root,
+                           relief = tk.RAISED,
+                           borderwidth = 1)
+    toggleFrame = tk.Frame(master = root,
+                           relief = tk.RAISED,
+                           borderwidth = 1)
+    submitFrame = tk.Frame(master = root,
                            relief = tk.RAISED,
                            borderwidth = 1)
     consoleFrame = tk.Frame(master = root,
@@ -143,8 +399,10 @@ if __name__ == '__main__':
     daq = RFSoC_Daq( displayFrame,
                      numChannels,
                      numSamples,
-                     sampleRate )
-    theDaq = daq
+                     sampleRate,
+                     figsize)
+    theDaq = daq   
+    
     displayFrame.pack( side = tk.TOP )
 
     buttons = {}
@@ -157,13 +415,44 @@ if __name__ == '__main__':
     buttons['User'] = tk.Button(buttonFrame,
                                 text = "User",
                                 command = defaultUserCommand)
+    
+    buttons['Reset'] = tk.Button(buttonFrame,
+                                text = "Reset",
+                                command = reload_script)
+    
+    
     buttons['Load'].pack( side = tk.LEFT )
     buttons['Acquire'].pack( side = tk.LEFT )
     buttons['User'].pack( side = tk.LEFT )
-
+    buttons['Reset'].pack( side = tk.LEFT )
+    
     buttonFrame.pack( side = tk.TOP )
+    
+    
+    toggles = {}
+    toggles["Freq"] = tk.Button(toggleFrame,
+                                    text="Freq", 
+                                    width=8, 
+                                    relief="raised",
+                                    command=lambda: toggle(toggles["Freq"], theDaq.setPlotFreq))
+    toggles["Fit"] = tk.Button(toggleFrame,
+                                    text="Fit", 
+                                    width=8, 
+                                    relief="raised",
+                                    command=lambda: toggle(toggles["Fit"], theDaq.setPlotFit))
+    
+    toggles["Freq"].pack( side = tk.LEFT )
+    toggles['Fit'].pack( side = tk.LEFT )
+    
+    toggleFrame.pack( side = tk.TOP )
+    
+    buttons['SetSampleSize'] = submitButton(root, submitFrame, "Set Sample Number (exponent of 2):", sampleExponent, lambda: submitNumSamples(buttons['SetSampleSize']))
+    buttons['SetSampleRate'] = submitButton(root, submitFrame, "Set Sample Rate (Redundant):", sampleRate, lambda: submitSampleRate(buttons['SetSampleRate']))
+    buttons['SetChannelCount'] = submitButton(root, submitFrame, "Set Number of Channels:", numChannels, lambda: submitNumberOfChannels(buttons['SetChannelCount']))   
+    
+    submitFrame.pack( side = tk.TOP )
 
-    locals = { 'daq' : daq,
+    locals = { 'daq' : theDaq,
                'buttons' : buttons }
     console = TextConsole( consoleFrame,
                            locals=locals )
@@ -174,5 +463,7 @@ if __name__ == '__main__':
     log = ScrolledLog( logFrame, logger )
     log.pack(fill='x', expand=True)
     logFrame.pack( fill='x', side = tk.TOP )
-
+    
+    rfsocLoad(hardware = "zcumts")
+        
     root.mainloop()
--- waveframe/Waveframe.py
+++ waveframe/Waveframe.py
@@ -1,9 +1,17 @@
+##Effectively a backup because I want to move some things arround and the what not
+
+##Python Imports
 import tkinter as tk
 from tkinter import ttk
 import numpy as np
+from scipy.fft import fft, ifft, fftfreq
+from scipy.constants import speed_of_light
+from scipy.optimize import curve_fit
 
+#System Imports
 import logging
 
+#Plotting imports
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
 from matplotlib.backend_bases import key_press_handler
@@ -14,58 +22,124 @@ class Waveframe(ttk.Notebook):
                  parent,
                  sampleRate=3.E9,
                  figsize=(3,2)):
+        
         self.sampleRate = sampleRate
+        
         super().__init__(parent)
-        self.td = ttk.Frame(self)
-        self.fd = ttk.Frame(self)
-        self.user = ttk.Frame(self)
-        # now add a matplotlib canvas in each one
+        
+        ##I've re-orded this section so it is a lot easier to 
         self.figs = {}
-        self.figs['time'] = Figure(figsize=figsize)
-        self.figs['freq'] = Figure(figsize=figsize)
-        self.figs['user'] = Figure(figsize=figsize)
         self.canvs = {}
+        
+        ##Set up the time figure
+        self.td = ttk.Frame(self)
+        self.figs['time'] = Figure(figsize=figsize)
         self.canvs['time'] = FigureCanvasTkAgg(self.figs['time'],
                                                master=self.td)
-        self.canvs['freq'] = FigureCanvasTkAgg(self.figs['freq'],
-                                               master=self.fd)
-        self.canvs['user'] = FigureCanvasTkAgg(self.figs['user'],
-                                               master=self.user)
         self.canvs['time'].draw()
         self.canvs['time'].get_tk_widget().pack()
+        self.add(self.td, text='Time')
+        
+        ##Set up the Fourier Transform Frequency Figure
+        self.fd = ttk.Frame(self)
+        self.figs['freq'] = Figure(figsize=figsize)
+        self.canvs['freq'] = FigureCanvasTkAgg(self.figs['freq'],
+                                               master=self.fd)
         self.canvs['freq'].draw()
         self.canvs['freq'].get_tk_widget().pack()
+        self.add(self.fd, text='Freq')
+        
+        ##Set up the fitted Waveform Figure
+        self.ad = ttk.Frame(self)
+        self.figs['fit'] = Figure(figsize=figsize)
+        self.canvs['fit'] = FigureCanvasTkAgg(self.figs['fit'],
+                                               master=self.ad)
+        self.canvs['fit'].draw()
+        self.canvs['fit'].get_tk_widget().pack()
+        self.add(self.ad, text='Fit')
+        
+        ##I don't know why this is here
+        self.user = ttk.Frame(self)
+        self.figs['user'] = Figure(figsize=figsize)
+        self.canvs['user'] = FigureCanvasTkAgg(self.figs['user'],
+                                               master=self.user)
         self.canvs['user'].draw()
         self.canvs['user'].get_tk_widget().pack()
-        self.add(self.td, text='Time')
-        self.add(self.fd, text='Freq')
         self.add(self.user, text='User')
+        
+        #Template for adding new things
+        
+        # self.new = ttk.Frame(self)
+        # self.figs['new'] = Figure(figsize=figsize)
+        # self.canvs['new'] = FigureCanvasTkAgg(self.figs['new'],
+        #                                        master=self.new)
+        # self.canvs['new'].draw()
+        # self.canvs['new'].get_tk_widget().pack()
+        # self.add(self.new, text='New')
+        
         # Callback signature is data, figure, canvas
         self.user_callback = None
-
-    def set_user_callback(self, fn):
-        if fn is None:
-            logging.debug("removing user_callback")
-            return
-        if callable(fn):
-            logging.debug("adding user_callback %s" % fn.__name__)
-            self.user_callback = fn
-
-    # Pass data to this, and it'll plot in
-    # time domain/freq domain/user pane
-    def plot(self, data):
+        
+    def convertToMag(self, yf):
+        N = len(yf)
+        return 2.0/N * np.abs(yf[0:N//2])
+        
+    def getSine(self, t,w,A,phi):
+        return A*np.sin((w*t)+phi)
+    
+    def doSineWaveFit(self, waveForm, Amplitude, Frequency, Phase):
+        guessFreq=Frequency
+        guessOmega=guessFreq*2*np.pi/10**9
+        guessAmp=Amplitude
+        guessPhase = Phase
+        
+        omega=freq=amp=phase=failCount=0
+        parameter = [guessOmega, guessAmp, guessPhase]
+        
+        parameter, covariance = curve_fit(self.getSine, np.arange(len(waveForm))*1.E9/self.sampleRate, waveForm, 
+                                          p0=[guessOmega,guessAmp,guessPhase], 
+                                          bounds=([guessOmega*0.9, guessAmp, guessPhase-0.25*np.pi], [guessOmega/0.9, guessAmp/0.7, guessPhase+0.25*np.pi]))
+        omega=parameter[0]
+        freq=parameter[0]/(2*np.pi)
+        amp=parameter[1]
+        phase=parameter[2]
+        return omega,freq,amp,phase
+        
+        
+    def plot(self, data, title, Wave, toPlot):#, Amp, Freq):
+        Amp = Wave[0]
+        Freq = Wave[1]
+        Phase = Wave[2]
+    
         self.figs['time'].clear()
         self.figs['freq'].clear()
+        self.figs['fit'].clear()
         self.figs['user'].clear()
         # we want this in nanoseconds, so divide samplerate by 1E9
         samplePeriod = 1.E9/self.sampleRate
         xaxis = np.arange(len(data))*samplePeriod
-        self.figs['time'].add_subplot(111).plot(xaxis, data)
+        
+        ax = self.figs['time'].add_subplot(111)
+        ax.plot(xaxis, data)
+        ax.set_title(title)
+        ax.set_xlabel('time (ns)')
+        ax.set_ylabel('ADC Counts', labelpad=-3.5)
+        
+        ax.axhline(y=0, color='black', linestyle='--', linewidth=0.5, label='Zero Line')
+        ax.axhline(y=Amp, color='black', linestyle='--', linewidth=0.3, label='Amplitude Line')
+        
+        stats_text = f"Amplitude: {Amp:.2f} Counts\nFrequency: {(Freq/10**6):.2f} MHz\nPhase: {Phase:.2f} radians"
+        ax.text(0.95, 0.95, stats_text, verticalalignment='top', horizontalalignment='right',
+            transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.5))
 
         self.canvs['time'].draw()
         
-        # figure out FFT stuff here
-        
+        ##Plotting additional plots
+        if toPlot[0]:
+            self.plotFreq(data, title, Amp, Freq)
+            
+        if toPlot[1]:
+            self.plotFit(data, title, xaxis, Amp, Freq, Phase)
 
         if callable(self.user_callback):
             try:
@@ -74,3 +148,43 @@ class Waveframe(ttk.Notebook):
                                    self.canvs['user'])
             except TypeError:
                 logging.error("user_callback '%s' type error: check arguments (data, fig, canvas)" % self.user_callback.__name__)        
+
+    ##Additional plots
+    def plotFreq(self, data, title, Amp, Freq):
+        scipy_fft = fft(data)
+        
+        xf = np.linspace(0.0, 1.0/(2.0/self.sampleRate), len(data)//2)/(10**6)
+        
+        axFreq = self.figs['freq'].add_subplot(111)
+        
+        axFreq.plot(xf, self.convertToMag(scipy_fft), label='scipy')
+        
+        axFreq.set_title(title)
+        axFreq.set_xlabel("Frequency (MHz)")
+        axFreq.set_ylabel("Magnitude (arb.)")
+        
+        axFreq.axvline(x=Freq/(10**6), color='r', linestyle='--',  linewidth=0.3, label='Frequency')
+        axFreq.axhline(y=Amp, color='r', linestyle='--', linewidth=0.3, label='Amplitude')
+        
+        axFreq.legend(loc='upper right')
+        
+        self.canvs['freq'].draw()
+        return 'Plotted frequency'
+    
+    def plotFit(self, data, title, xaxis, Amp, Freq, Phi):
+        
+        Omega,Frequency,Amplitude,Phase=self.doSineWaveFit(data, Amp, Freq, Phi)
+                        
+        axFit = self.figs['fit'].add_subplot(111)
+        axFit.plot(xaxis, data, label='Data')
+        axFit.plot(xaxis, self.getSine(xaxis,Omega,Amplitude,Phase), label='Fit')
+        axFit.set_title(title)
+        axFit.set_xlabel('time (ns)')
+        axFit.set_ylabel('ADC Counts', labelpad=-3.5)
+        
+        axFit.axhline(y=0, color='black', linestyle='--', linewidth=0.5, label='Zero Line')
+        axFit.axhline(y=Amp, color='black', linestyle='--', linewidth=0.3, label='Amplitude Line')
+        
+        axFit.legend(loc='upper right')
+        
+        self.canvs['fit'].draw()
